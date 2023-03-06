f3a9b51bb73e239584a7d09510b8b58e7b186151
AmoraMas <amoramas@protonmail.com>
2023-03-06 14:20:02 -0700

    migration delays

--- migration.js
+++ migration.js
@@ -5,7 +5,7 @@ const pool = dbConn.getPool();
 
 
 // establish a connection we can close with a callback
-function runMigrations(pool, callback){
+function runMigrations1(pool, callback){
     // connect to DB
     pool.connect((err, client, done) => {
         if (err) {
@@ -14,7 +14,6 @@ function runMigrations(pool, callback){
             return done();
         }
 
-        // run migration SQL
         pool.query(`CREATE TABLE IF NOT EXISTS locations (
             id SERIAL PRIMARY KEY NOT NULL,
             country VARCHAR(25),
@@ -31,6 +30,18 @@ function runMigrations(pool, callback){
                 }
             }
         );
+    });
+}
+
+// establish a connection we can close with a callback
+function runMigrations2(pool, callback){
+    // connect to DB
+    pool.connect((err, client, done) => {
+        if (err) {
+            console.log("Failed to connect to the database");
+            console.error(err);
+            return done();
+        }
 
         pool.query(`CREATE TABLE IF NOT EXISTS accounts (
             id SERIAL PRIMARY KEY NOT NULL,
@@ -91,6 +102,18 @@ function runMigrations(pool, callback){
                 }
             }
         );
+    });
+}
+
+// establish a connection we can close with a callback
+function runMigrations3(pool, callback){
+    // connect to DB
+    pool.connect((err, client, done) => {
+        if (err) {
+            console.log("Failed to connect to the database");
+            console.error(err);
+            return done();
+        }
 
         pool.query(`CREATE TABLE IF NOT EXISTS places (
             id SERIAL PRIMARY KEY NOT NULL,
@@ -113,6 +136,18 @@ function runMigrations(pool, callback){
                 }
             }
         );
+    });
+}
+
+// establish a connection we can close with a callback
+function runMigrations4(pool, callback){
+    // connect to DB
+    pool.connect((err, client, done) => {
+        if (err) {
+            console.log("Failed to connect to the database");
+            console.error(err);
+            return done();
+        }
 
         pool.query(`CREATE TABLE IF NOT EXISTS dreams (
             id SERIAL PRIMARY KEY NOT NULL,
@@ -128,6 +163,18 @@ function runMigrations(pool, callback){
                 }
             }
         );
+    });
+}
+
+// establish a connection we can close with a callback
+function runMigrations5(pool, callback){
+    // connect to DB
+    pool.connect((err, client, done) => {
+        if (err) {
+            console.log("Failed to connect to the database");
+            console.error(err);
+            return done();
+        }
 
         pool.query(`CREATE TABLE IF NOT EXISTS visits (
             id SERIAL PRIMARY KEY NOT NULL,
@@ -149,7 +196,19 @@ function runMigrations(pool, callback){
 }
 
 
-runMigrations(pool, () => {
-    // migrations are complete, we can close the pool
-    pool.end();
+runMigrations1(pool1, () => {
+    runMigrations2(pool2, () => {
+        pool1.end();
+        runMigrations3(pool3, () => {
+            pool2.end();
+            runMigrations4(pool4, () => {
+                pool3.end();
+                runMigrations5(pool5, () => {
+                    // migrations are complete, we can close the pools
+                    pool4.end();
+                    pool5.end();
+                })
+            })
+        })
+    })
 })
